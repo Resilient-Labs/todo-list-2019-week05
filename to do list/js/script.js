20fe9e949e7c4ef94cae36ba9296f701f8f0ba63
Mikey Neely <mike.neely90@gmail.com>
2019-06-23 23:23:29 -0400

    second commit

--- to do list/js/script.js
+++ to do list/js/script.js
@@ -1,20 +1,23 @@
 const button = document.querySelector('button');
 const input = document.querySelector('input[type="text"]');
 const list = document.querySelector('ul');
+
 button.addEventListener('click', () => {
   if (input.value != '') {
     const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.classList.add('deleteFromList');
     li.appendChild(document.createTextNode(input.value));
-    span.appendChild(document.createTextNode('x'));
-    li.appendChild(span);
     list.appendChild(li);
     input.value = '';
   }
 });
 list.addEventListener('click', (e) => {
-  if(e.target.classList.contains('deleteFromList') === true) {
-    e.target.parentElement.remove();
+  if (e.target.tagName === "LI"){
+    e.target.classList.toggle("checked")
  }
 });
+function removeAll(){
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+  }
+}
+document.getElementById('deleteAll').addEventListener('click', removeAll);
