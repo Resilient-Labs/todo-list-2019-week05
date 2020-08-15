da5d520f2a9eca29547569d1e78c319eade1b95a
Anastasia Yim <yim.anast@gmail.com>
2020-08-15 00:27:58 -0400

    Created a To-Do List using JavaScript

--- JS/Todo.js
+++ JS/Todo.js
@@ -6,7 +6,9 @@ const inputValue = document.getElementById('myInput');
 let remain = document.querySelector('.remaining');
 let remainder = 1;
 
-function addList() {
+
+function addList(e) {
+  e.preventDefault();
   if (inputValue.value === ''){
     return alert("ENTER SOMETHING")
   }
@@ -23,82 +25,39 @@ function addList() {
 }
 addBtn.addEventListener('click', addList)
 
+
 //Clear ALL list items button
 let clearList = document.querySelector('.clearAll');
 
 function clearAll() {
+  remainder = 1;
   document.getElementById('myUL').innerHTML = "";
+  document.querySelector('.remaining').innerHTML = "0";
 }
 clearList.addEventListener('click', clearAll)
 
-/*const checkMark =
-
-function checkDone() {
-  document.addEventListener('click', checkDone)
-}*/
-
-/*let remain = document.querySelector('.remaining').value;
-let remainder = 0; */
 
 function crossoutLi(e) {
   e.target.classList.toggle('strikeThrough')
 
   if (e.target.classList.contains('strikeThrough')){
     e.target.classList.toggle('.strikeThrough');
-    /*let count = remainder--
-    remain.innerHTML = count.toString()
-      console.log(remain)*/
+
     document.querySelector('.remaining').innerText = parseInt(document.querySelectorAll('.toDo').length) - document.querySelectorAll('.strikeThrough').length
   }
 }
 ul.addEventListener('click', crossoutLi)
 
 
-//CHECK BOXES
-/*const completedTask = document.createElement('INPUT');
-completedTask.setAttribute('type', 'checkbox');
-li.appendChild(completedTask);
-completedTask.setAttribute('id', 'box');*/
-
-
 
 let clearDone = document.querySelector('.clearCom');
 
-  function clearCom() {
-    document.querySelectorAll('.toDo').forEach((item,) => {
-      if (item.classList.contains('strikeThrough')) {
-        item.parentNode.removeChild(item);
-      }
-    });
-  }
-clearDone.addEventListener('click', clearCom)
-
-/* let remain = document.querySelector('.remaining').value;
-let remainder = 0;
-
-function remainingItems(e) {
-  console.log('help')
-  if (e.target.classList.contains('.toDo')){
-    e.target.classList.toggle('strikeThrough');
-    remainder++
-    document.querySelector('.remaining').innerText = document.querySelector('myUL').length - document.querySelectorAll('.strikeThrough').length
-  }
+function clearCom() {
+  remainder = 1;
+  document.querySelectorAll('.toDo').forEach((item,) => {
+    if (item.classList.contains('strikeThrough')) {
+      item.parentNode.removeChild(item);
+    }
+  });
 }
-  remainingItems.addEventListener() */
-
-//Vonds helped :)
-
-/* let activityRemain = document.querySelector('.remaining');
-  let activityRemain = 0;
-
-  function remainingAct(){
-
-  }
-  /*function logKey(e){
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("myBtn").click();
-    }*/
+clearDone.addEventListener('click', clearCom)
