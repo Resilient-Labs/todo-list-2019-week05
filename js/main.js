b08efe2abf67ca27e58980e437e7f5724a167acc
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-08-17 23:08:30 -0400

    Updates
    Add more JavaScript code and CSS styling

--- js/main.js
+++ js/main.js
@@ -4,21 +4,27 @@ form.addEventListener('submit',addItem);
 let clearAll= document.querySelector('#clearAll');
 clearAll.addEventListener('click', clearToDoList);
 toDoList.addEventListener('click', toggleCompleted)
+//Tell document to find the element with the id associated with the button in html and assign that value to the variable clearCompleteButton
+let clearCompleteButton = document.querySelector('#clearComplete');
+//Tell the clearCompleteButton to listen for a click and to run the clearComplete function
+clearCompleteButton.addEventListener('click', clearComplete);
+
+const toDoCounter = document.getElementById('toDo')
 
 function addItem(e) {
   e.preventDefault()
   document.querySelector('input').value;
   let li = document.createElement('li');
-  let addLi =
-  document.createTextNode(document.querySelector('input').value);
+  let addLi = document.createTextNode(document.querySelector('input').value);
   li.appendChild(addLi);
   toDoList.appendChild(li);
-  document.getElementById('toDo').innerText = toDoList.getElementsByTagName("li").length;
+  toDoCounter.innerText = toDoList.getElementsByTagName("li").length;
 }
 
 function clearToDoList() {
   while (toDoList.firstChild){
     toDoList.removeChild(toDoList.firstChild);
+    toDoCounter.innerText = toDoList.getElementsByTagName("li").length;
   }
 }
 
@@ -27,11 +33,9 @@ function toggleCompleted(e) {
   e.target.classList.toggle('crossOut');
 }
 function clearComplete() {
-  while (toDoList.firstChild){
-    if () {
-      //check the class of the child with crossOut
-    }
-    toDoList.removeChild(toDoList.firstChild)
+  const crossedOutItems = document.getElementsByClassName('crossOut')
+  while (crossedOutItems.length){
+    toDoList.removeChild(crossedOutItems[0]);
+    toDoCounter.innerText = toDoList.getElementsByTagName("li").length;
   }
 }
-// decrease the count of 'toDo' to reflect the remaining total
