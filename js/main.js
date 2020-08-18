8da545de356530680ae1a7fce02a7d7178532773
Joshua Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2020-08-18 21:08:47 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let input = document.getElementById('input');
+function countLi(){
+	let len = document.getElementById('list').getElementsByTagName('li');
+  let lilength = len.length
+  document.getElementById('leftToDo').innerHTML = (`you have ${lilength} tasks`);
+}
+function clicky(e){
+  if (e.target.classList.contains('frank')) {
+    e.target.classList.toggle('checked')
+  }
+}
+document.getElementById('list').addEventListener('click',clicky)
+function addText(){
+  let userInput = document.getElementById('input').value;
+  if (userInput !== '') {
+    document.getElementById('list').innerHTML += (`<li class="frank" onclick="clicky()">${userInput}</li>`);
+    input.value = ('');
+    countLi()
+  }
+}
+function clearC(){
+  let frank2 = document.querySelectorAll('li').length - document.querySelectorAll('.checked').length;
+  document.querySelectorAll('.checked').forEach(x=>x.innerHTML='');
+  document.getElementById('leftToDo').innerHTML = (`you have ${frank2} tasks`);
+}
+function clearA(){
+  document.getElementById('list').innerHTML = ('')
+  document.getElementById('leftToDo').innerHTML = (`you have 0 tasks`)
+}
