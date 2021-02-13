b1faf32043a2f41be3373fea608904b39af3eb23
Juan Vistro <jpmbvistro@gmail.com>
2021-02-13 17:04:05 -0500

    side panel

--- js/main.js
+++ js/main.js
@@ -181,6 +181,18 @@ function removeFocus(event) {
   event.currentTarget.readOnly = true
 }
 
+/**
+Aside panel functionality
+**/
+document.querySelector('.info-button').addEventListener('click', toggleAside)
+document.querySelector('#hide-aside').addEventListener('click', toggleAside)
+
+function toggleAside(){       document.querySelector('aside').classList.toggle('reveal')
+}
+/**
+Aside Panel end
+*/
+
 
 // /*
 //   Task Constructor
