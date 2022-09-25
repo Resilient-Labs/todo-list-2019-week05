2646ba6ea78d7518c0b8b7c1d069188e3a6dd17b
Ty Phillips <phillipstashan22@gmail.com>
2022-09-25 16:41:34 -0400

    Created a todo list that you can list items and cross them off

---
+++ todo.js
@@ -0,0 +1,39 @@
+//TO DO LIST
+
+
+//target buttons in dom and addevent listeners
+document.querySelector('#click').addEventListener('click', listedItems)
+document.querySelector('#list').addEventListener('click', cancel)
+
+
+//create a function that creates a listed item in the dom
+function listedItems() {
+    //console log the value 
+    //declare the value of the input
+    let textBoxText = document.querySelector('#input').value 
+    //console.log(textBoxText)
+    //let ulElement = document.querySelector('#list')
+    //newListItem does not exist in the dom yet
+    let newListItem = document.createElement('li')
+    let text = document.createTextNode(textBoxText)
+    newListItem.appendChild(text)
+    let ul = document.querySelector('#list')
+    ul.appendChild(newListItem)
+    //ulElement.appendChild(newListItem)
+    //removeChild(document.createTextNode(textBoxText))
+    //newListItem.addEventListener('click', cancel)
+}
+
+
+//create a function that toggles a line through completed item
+function cancel(click) {
+    /*document.getElementById('#list').toggleAttribute('#list')
+    list.appendChild(list.childn)*/
+    click.target.classList.toggle('soulBreaker')
+}
+
+
+
+
+
+//clear button
