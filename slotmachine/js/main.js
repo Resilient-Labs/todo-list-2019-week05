b46cb05b16fd62a3e22020c5e83f37a1af69f926
Christina <christina.brgs@gmail.com>
2023-03-17 14:30:29 -0400

    this was challenging but it runs

--- slotmachine/js/main.js
+++ slotmachine/js/main.js
@@ -1,35 +1,78 @@
 
-const slotOne = document.querySelector('#slotOne')
-const slotTwo = document.querySelector('#slotTwo')
-const slotThree = document.querySelector('#slotThree')
-const minButton = document.querySelector('#minButton')
-const maxButton = document.querySelector('#maxButton')
-const counter = document.querySelector('#count')
+const slotOne = document.querySelector('#reel1')
+const slotTwo = document.querySelector('#reel2')
+const slotThree = document.querySelector('#reel3')
+let amount =  document.querySelector('#amount')
 
-// let symbols = ['7️⃣', '❤️', '⭐', '♠️', '💎']
-let symbols = ['7', 'i', '8', '0', 'd']
+// Money bank
+let moneyBankYo = 5000
+// Prize 
+let prize = 50000
+// minBet
+let minBet = 20
+//maxBet
+let maxBet = 100
+// lost
+let lost = -60
+
+const slots = ['img/1.png', 'img/2.png', 'img/3.png', 'img/4.jpg', 'img/5.png']
 
 
-minButton.addEventListener('click', getRandomSymbol)
-maxButton.addEventListener('click', getRandomSymbol)
 
-// Generates a random symbol
-function getRandomSymbol(symbols) {
-    let count = 0
-    let random = Math.floor(Math.random() * symbols.length)
+document.querySelector('#minButton').addEventListener('click', pullTheLever)
+document.querySelector('#maxButton').addEventListener('click', pullTheLever)
 
-    slotOne.innertext = symbols[random]
-    slotTwo.innertext = symbols[random]
-    slotThree.innertext = symbols[random]
 
-        if (slotOne.innertext === slotTwo.innertext && slotTwo == slotThree.innertext) {
-            alert ('YOU WON') 
-            count = + 50
-        }
-        else if (slotOne.innertext !== slotTwo.innertext !== slotThree.innertext) {
-            alert ('LOSER')
-            count = - 50
-        }
 
-        counter.innertext = count
+amount.innerText = moneyBankYo
+
+function pullTheLever(event) {
+    let negativeMoney = Number(event.target.value)
+    if (moneyBankYo < negativeMoney) {
+        alert ('MEH')
+        document.querySelector('#display').innerText = ""
+    } else { moneyBankYo -= negativeMoney 
+        amount.innerText = moneyBankYo
+        returnPicture()
+    }
+
 }
+
+function getRandomPicture() {
+    let picture = Math.floor(Math.random()*5) 
+    return picture
+}
+
+function returnPicture () {
+    let firstNumber = getRandomPicture()
+    let secondNumber = getRandomPicture()
+    let thirdNumber = getRandomPicture ()
+
+    slotOne.src = slots[firstNumber]
+    slotTwo.src = slots[secondNumber]
+    slotThree.src = slots[thirdNumber]
+ 
+    win (firstNumber, secondNumber, thirdNumber)
+}
+
+
+function win (firstNumber, secondNumber, thirdNumber) {
+    if (firstNumber == secondNumber && secondNumber == thirdNumber) {
+    document.querySelector('#display').innerText = 'Won'
+    youWinMoney()
+    } else { document.querySelector('#display').innerText = 'Loser'}
+}
+   
+
+function youWinMoney() {
+    moneyBankYo += prize
+   amount.innerText = moneyBankYo
+
+
+}
+
+
+
+
+
+
