f1c00be273952c590e8ddd6475e8fd93ff41e1cf
Christina <christina.brgs@gmail.com>
2023-03-16 20:57:35 -0400

    updated

--- slotmachine/js/main.js
+++ slotmachine/js/main.js
@@ -3,15 +3,19 @@ const slotOne = document.querySelector('#slotOne')
 const slotTwo = document.querySelector('#slotTwo')
 const slotThree = document.querySelector('#slotThree')
 const minButton = document.querySelector('#minButton')
+const maxButton = document.querySelector('#maxButton')
+const counter = document.querySelector('#count')
 
 // let symbols = ['7️⃣', '❤️', '⭐', '♠️', '💎']
 let symbols = ['7', 'i', '8', '0', 'd']
 
 
 minButton.addEventListener('click', getRandomSymbol)
+maxButton.addEventListener('click', getRandomSymbol)
 
 // Generates a random symbol
 function getRandomSymbol(symbols) {
+    let count = 0
     let random = Math.floor(Math.random() * symbols.length)
 
     slotOne.innertext = symbols[random]
@@ -20,15 +24,12 @@ function getRandomSymbol(symbols) {
 
         if (slotOne.innertext === slotTwo.innertext && slotTwo == slotThree.innertext) {
             alert ('YOU WON') 
+            count = + 50
         }
         else if (slotOne.innertext !== slotTwo.innertext !== slotThree.innertext) {
             alert ('LOSER')
+            count = - 50
         }
 
-}
-
-
-function count() {
-    let count = 0
-
+        counter.innertext = count
 }
