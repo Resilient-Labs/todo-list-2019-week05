faac8d25ea834cfcabca542a85e891064903a8e2
Koshin-M-Mohamed <koshin.m.mohamed@gmail.com>
2023-03-17 00:54:54 -0400

    finished todo list

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
 document.querySelector("#btn").addEventListener('click', ()=>{ //function without name
     let userInput = document.querySelector("#input").value
-    document.querySelector("#ul").innerHTML += `<li><span class="item">${userInput}</span> <span id="x" class="x">X</span></li>`
+    document.querySelector("#ul").innerHTML += `<li id='listItem'>${userInput} </li>`
+    getCount()
 
-    document.querySelectorAll("#x").forEach(item=>{
-        item.addEventListener('click', deletLI)
+    document.querySelectorAll('#listItem').forEach(item=>{
+        item.addEventListener('click', complete)
     })
 
     document.querySelector("#input").value = ""
+
+    document.querySelector('#clearAll').addEventListener('click', clearAll)
+    document.querySelector('#clearCompleted').addEventListener('click', clearCompleted)
     
-})
+    })
+    
 
 
-function deletLI(e){
+function clearAll(){
+    document.querySelector("#ul").innerHTML = ''
+    getCount()
+
+}
+
+function clearCompleted(){
+    document.querySelectorAll('.complete').forEach(item=>{ 
+        item.remove()
+        
+
+    } )
+    getCount()
+
+}
+
+
+function complete(e){
     //get the event information
-    // delete the parentNode which is the Li
-    e.target.parentElement.remove()
+    // add the css class called complete to the LI the user clicked
+    //this will make the LI crossed out and change it's background color 
+    e.target.classList.add('complete')
+}
+
+function getCount(){
+    let count = 0
+    document.querySelectorAll('li').forEach(item=>{
+        count++
+ })
+    document.querySelector('#count').innerHTML = count
 }
