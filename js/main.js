f49670e31a4b113483d0180bfb73cf9eae090d60
AmaurySilverio <amaurycodes@gmail.com>
2023-03-19 23:02:52 -0400

    todoList

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// VARIABLES
+const inputValue = document.getElementById("input");
+const addButton = document.querySelector(".addButton");
+const clear = document.querySelector(".clear");
+const clearAll = document.querySelector(".clearAll");
+const list = document.querySelector(".list");
+const numOfItems = document.getElementById("numOfItems");
+let updatedCount = 0;
+
+// FUNCTIONS
+const addItem = () => {
+  let input = inputValue;
+  if (input.value) {
+    let item = document.createElement("li");
+    item.innerText = input.value;
+    list.appendChild(item);
+    input.value = "";
+    updatedCount++;
+    numOfItems.innerHTML = updatedCount;
+    crossOut(item);
+  } else {
+    console.log(input.value);
+    return alert("write something");
+  }
+};
+
+const crossOut = (e) => e.target.classList.toggle("crossOut");
+
+const clearItAll = () => {
+  document.querySelectorAll("li").forEach((li) => li.remove());
+  updatedCount = 0;
+  numOfItems.innerHTML = updatedCount;
+};
+
+const clearItem = () => {
+  let liList = document.querySelectorAll(".crossOut");
+  let liNumbered = 0;
+  if (liList) {
+    liNumbered = liList.length;
+    document.querySelectorAll(".crossOut").forEach((li) => li.remove());
+    updatedCount = updatedCount - liNumbered;
+    numOfItems.innerHTML = updatedCount;
+  } else {
+    return;
+  }
+};
+// EVENT LISTENERS
+addButton.addEventListener("click", addItem);
+list.addEventListener("click", crossOut);
+clearAll.addEventListener("click", clearItAll);
+clear.addEventListener("click", clearItem);
